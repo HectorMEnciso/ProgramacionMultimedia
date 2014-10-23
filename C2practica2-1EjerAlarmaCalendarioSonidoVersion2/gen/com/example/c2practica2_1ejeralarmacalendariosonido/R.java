00ce914d61f42d315a6cb83207e64a5cd0cd7d96
HectorMEnciso <hectorencisolezcano@gmail.com>
2014-10-23 17:03:56 +0200

    Héctor Enciso

--- C2practica2-1EjerAlarmaCalendarioSonidoVersion2/gen/com/example/c2practica2_1ejeralarmacalendariosonido/R.java
+++ C2practica2-1EjerAlarmaCalendarioSonidoVersion2/gen/com/example/c2practica2_1ejeralarmacalendariosonido/R.java
@@ -1071,9 +1071,10 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int Horas=0x7f060042;
+        public static final int Horas=0x7f060046;
         public static final int Minutos=0x7f060041;
         public static final int NombreAlarma=0x7f06003f;
+        public static final int SonidoRamdom=0x7f060047;
         public static final int action_bar=0x7f06001c;
         public static final int action_bar_activity_content=0x7f060015;
         public static final int action_bar_container=0x7f06001b;
@@ -1088,9 +1089,10 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f060025;
         public static final int always=0x7f06000b;
         public static final int beginning=0x7f060011;
-        public static final int botonAlarma=0x7f06003e;
+        public static final int botonAlarma=0x7f060042;
         public static final int botonCalendario=0x7f06003d;
-        public static final int botonSonido=0x7f060040;
+        public static final int botonSonido=0x7f060043;
+        public static final int botonSonidoDos=0x7f060044;
         public static final int checkbox=0x7f06002d;
         public static final int collapseActionView=0x7f06000d;
         public static final int default_activity_button=0x7f060028;
@@ -1133,9 +1135,9 @@ containing a value of this type.
         public static final int submit_area=0x7f060039;
         public static final int tabMode=0x7f060002;
         public static final int textView1=0x7f06003c;
-        public static final int textView2=0x7f060043;
-        public static final int textView3=0x7f060044;
-        public static final int textView4=0x7f060045;
+        public static final int textView2=0x7f060040;
+        public static final int textView3=0x7f060045;
+        public static final int textView4=0x7f06003e;
         public static final int title=0x7f06002b;
         public static final int top_action_bar=0x7f060020;
         public static final int up=0x7f060021;
@@ -1196,7 +1198,11 @@ containing a value of this type.
         public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class raw {
-        public static final int kalimba=0x7f050000;
+        public static final int applause=0x7f050000;
+        public static final int babybabble=0x7f050001;
+        public static final int clearingthroat=0x7f050002;
+        public static final int comeon=0x7f050003;
+        public static final int footsteps=0x7f050004;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
