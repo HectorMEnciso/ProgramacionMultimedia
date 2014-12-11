b0eb3d1748cf8fc41283287a5f422c25b9f88cdf
HectorMEnciso <hectorencisolezcano@gmail.com>
2014-12-11 19:21:06 +0100

    Héctor Enciso

--- P33ejer62/app/src/main/java/com/example/dam2015/p33ejer6/MainActivity.java
+++ P33ejer62/app/src/main/java/com/example/dam2015/p33ejer6/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.dam2015.p33ejer6;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.content.Intent;
@@ -13,7 +18,7 @@ public class MainActivity extends Activity {
     private TextView lblAutor;
     private TextView lblDuracion;
     private ListView LstOpciones;
-
+    adaptadorCanciones adaptador;
     private Cancion[] datos = new Cancion[]{
             new Cancion(R.drawable.rihanna,"Título 1", "autor1", "3:20"),
             new Cancion(R.drawable.rihanna,"Título 2", "autor2", "4:30"),
@@ -26,15 +31,72 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adaptadorCanciones adaptador = new adaptadorCanciones(this,datos);
+         adaptador = new adaptadorCanciones(this,datos);
 
        // lblTitulo = (TextView) findViewById(R.id.lblTitulo);
       //  lblAutor = (TextView) findViewById(R.id.lblAutor);
      //   lblDuracion = (TextView) findViewById(R.id.lblDuracion);
         LstOpciones= (ListView) findViewById(R.id.LstOpciones);
         LstOpciones.setAdapter(adaptador);
+        registerForContextMenu(LstOpciones);
+    }
+    //Generacion del menu a partir del menu_main.xml
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+
+
+        if(v.getId() == R.id.LstOpciones){
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+
+            menu.setHeaderTitle(LstOpciones.getAdapter().getItem(info.position).toString());
+
+            inflater.inflate(R.menu.opciones_elementos, menu);
+        }
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info =(AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.EliminarSeleccionada:
+
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.AnadirCancion:
+
+                return true;
+            case R.id.BorrarTodas:
+
+                adaptador.clear();
+                LstOpciones.notifyAll();
+                adaptador.notifyDataSetChanged();
+                return true;
+            case R.id.GuardarFichero:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     public void onClick(View v){
     Intent i=new Intent(this,AnadirCancion.class);
         startActivityForResult(i,1);
